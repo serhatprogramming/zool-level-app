e500225fb91f89baffb93bfc537c64fa723226a5
serhatprogramming <serhatprogramming@gmail.com>
2023-08-26 10:33:38 -0400

    zoom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 const App = () => {
-  return <div>Hello World!</div>;
+  const zoomLevel = 0;
+  return (
+    <div>
+      <h2>Zoom Level App</h2>
+
+      <button>Zoom In</button>
+      <button>Zoom Out</button>
+      <button>Reset Zoom</button>
+      <p>Zoom Level: {zoomLevel}</p>
+    </div>
+  );
 };
 
 export default App;
